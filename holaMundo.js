a245f9d3a1c97ed18fbb1e1cc1fa864b517ad7ea
domgarlor[user] <domgarlor@gmail.com>
2016-12-20 16:15:31 +0100

    Añadidos mádulos --dev para react y webpack

---
+++ holaMundo.js
@@ -0,0 +1,24 @@
+
+console.log('Hola')
+
+
+/*
+
+class MiComponente extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { vez: 0, };
+    }
+    handleClick() {
+        this.setState({
+            vez: this.state.vez + 1,
+        });
+    }
+    render() {
+        return (<h1 id='titulo' onclick={this.handleClick}> Hola {this.props.nombre}  {this.state.vez} </h1>);
+    }
+
+}
+
+ReactDOM.render(<MiComponente nombre="Domingo" />, document.getElementById('app'));
+*/
